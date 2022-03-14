354b5e8721c085e9dced52b767755772e3247185
Vincent Wijshoff <vincent.wijshoff@student.kuleuven.be>
2022-03-14 15:50:43 +0100

    start of graph based structure

---
+++ src/main/java/agent/behavior/behavior/FindDestnation.java
@@ -0,0 +1,18 @@
+package agent.behavior.behavior;
+
+import agent.AgentAction;
+import agent.AgentCommunication;
+import agent.AgentState;
+import agent.behavior.Behavior;
+
+public class FindDestnation extends Behavior {
+    @Override
+    public void communicate(AgentState agentState, AgentCommunication agentCommunication) {
+
+    }
+
+    @Override
+    public void act(AgentState agentState, AgentAction agentAction) {
+
+    }
+}
---
+++ src/main/java/agent/behavior/behaviorchange/FoundDestination.java
@@ -0,0 +1,15 @@
+package agent.behavior.behaviorchange;
+
+import agent.behavior.BehaviorChange;
+
+public class FoundDestination extends BehaviorChange {
+    @Override
+    public void updateChange() {
+
+    }
+
+    @Override
+    public boolean isSatisfied() {
+        return false;
+    }
+}
