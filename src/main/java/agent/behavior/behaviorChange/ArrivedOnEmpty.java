5f9f77aff09da6ff1926151075b44603af3e41cd
Toon Van Hove <toonvanhove@hotmail.com>
2022-04-07 15:03:22 +0200

    Make sure agent enter Charge behaviour when arriving at EnergyStation

--- src/main/java/agent/behavior/behaviorChange/ArrivedOnEmpty.java
+++ src/main/java/agent/behavior/behaviorChange/ArrivedOnEmpty.java
@@ -8,19 +8,23 @@ import java.util.Objects;
 public class ArrivedOnEmpty extends BehaviorChange {
 
     private boolean arrived;
+    private boolean isChargingStation;
 
     public ArrivedOnEmpty(){
         arrived = false;
+        isChargingStation = false;
     }
+
 
     @Override
     public void updateChange() {
         int[] target = Objects.requireNonNull(Memory.getTarget(getAgentState()));
         this.arrived = target[0] == getAgentState().getX() && target[1] == getAgentState().getY();
+        this.isChargingStation = getAgentState().getPerception().getCellPerceptionOnAbsPos(getAgentState().getX(), getAgentState().getY() + 1).containsEnergyStation();
     }
 
     @Override
     public boolean isSatisfied() {
-        return arrived;
+        return arrived && !isChargingStation;
     }
 }
